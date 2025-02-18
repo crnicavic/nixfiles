0910b98907312e0380e33fb02b44ade0f7c66ce1
crnicavic <cavic5000@tutanota.com>
2025-02-18 11:27:28 +0100

    start fresh

--- nixos/modules/docker.nix
+++
@@ -1,4 +0,0 @@
-{ pkgs, ... }: {
-	virtualisation.docker.enable = true;
-	users.users.user.extraGroups = ["docker"];
-}
--- nixos/modules/xserver.nix
+++
@@ -1,6 +0,0 @@
-{
-	services.xserver.enable = true;
-	services.autorandr.enable = true;
-	services.xserver.xkb.layout = "us";
-	services.xserver.xkb.options = "eurosign:e,caps:escape";
-}
