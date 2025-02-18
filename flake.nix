4cd012a9fc02a2866042bf2f256b25b36506fe85
crnicavic <cavic5000@tutanota.com>
2025-02-18 16:16:11 +0100

    succesful system build!

--- flake.nix
+++ flake.nix
@@ -1,16 +1,19 @@
 {
   description = "A very basic flake";
 
-  inputs = {
-    nixpkgs_unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-	nixpkgs.url = "github:nixos/nixpkgs";
-	nixpkgs_2405.url = "github:nixos/nixpkgs?rev=1bde3e8e37a72989d4d455adde764d45f45dc11c";
-  };
+	inputs = {
+		nixpkgs_unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+		nixpkgs.url = "github:nixos/nixpkgs";
+		nixpkgs_2405.url = "github:nixos/nixpkgs?rev=1bde3e8e37a72989d4d455adde764d45f45dc11c";
+	};
 
-  outputs = { nixpkgs, ... } @ inputs: {
-    packages.x86_64-linux.hello_2405 = inputs.nixpkgs_2405.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = inputs.nixpkgs.legacyPackages.x86_64-linux.hello;
-
-  };
+	outputs = { nixpkgs, ... } @ inputs: {
+		nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+			specialArgs = {};
+			modules =
+			[
+				./configuration.nix
+			];
+		};
+	};
 }
