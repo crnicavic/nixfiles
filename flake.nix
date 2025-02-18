e36649668c93a77c1d3980213ea978977e51dbdd
crnicavic <cavic5000@tutanota.com>
2025-02-18 15:46:57 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -2,14 +2,15 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs_unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+	nixpkgs.url = "github:nixos/nixpkgs";
+	nixpkgs_2405.url = "github:nixos/nixpkgs?rev=1bde3e8e37a72989d4d455adde764d45f45dc11c";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { nixpkgs, ... } @ inputs: {
+    packages.x86_64-linux.hello_2405 = inputs.nixpkgs_2405.legacyPackages.x86_64-linux.hello;
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+    packages.x86_64-linux.default = inputs.nixpkgs.legacyPackages.x86_64-linux.hello;
 
   };
 }
