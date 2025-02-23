ba6c0b7f278a81204493530b985e3e63d2a34643
crnicavic <cavic5000@tutanota.com>
2025-02-23 18:19:59 +0100

    change file structure

---
+++ hosts/pc-conf.nix
@@ -0,0 +1,46 @@
+{ config, lib, pkgs, ... }: {
+	imports = [ 
+		# Include the results of the hardware scan.
+		./hardware/pc-hardware-conf.nix
+		../packages.nix
+		../modules/bundle.nix
+	];
+
+	# Use the systemd-boot EFI boot loader.
+	boot.loader.systemd-boot.enable = true;
+	boot.loader.efi.canTouchEfiVariables = true;
+		
+	networking.hostName = "nixpc";
+	networking.networkmanager.enable = true;
+
+	boot.initrd.kernelModules = [ "amdgpu" ];
+
+	hardware.bluetooth.enable = true;
+	hardware.bluetooth.powerOnBoot = true;
+
+	hardware.graphics.enable32Bit = true;	
+
+	services.gvfs.enable = true;
+
+	nix.settings.experimental-features = [ "nix-command" "flakes"];
+	
+	# Set your time zone.
+	time.timeZone = "Europe/Belgrade";
+
+	# Enable touchpad support (enabled default in most desktopManager).
+	services.libinput.enable = true;
+
+	 # Define a user account. Don't forget to set a password with ‘passwd’.
+	users.users.user = {
+		isNormalUser = true;
+		extraGroups = [ "wheel" "audio" "video" "docker" "networkmanager"]; # Enable ‘sudo’ for the user.
+	};
+	fileSystems."/".options = [ "noatime" "nodiratime" "discard" ];
+	
+	environment.variables.PATH = [ "/usr/local/bin" ];
+	
+	system.stateVersion = "24.05"; 
+	
+	system.autoUpgrade.enable = true;
+}
+
