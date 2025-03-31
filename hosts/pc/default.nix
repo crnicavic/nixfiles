08927565d25f81a88b5126d6388a24b65367902a
crnicavic <cavic5000@tutanota.com>
2025-03-31 10:39:11 +0200

    add duckdns to server, nixos actually has a fucking option for it

--- hosts/pc/default.nix
+++ hosts/pc/default.nix
@@ -11,7 +11,6 @@
 	boot.loader.systemd-boot.enable = true;
 	boot.loader.efi.canTouchEfiVariables = true;
 
-	networking.firewall.enable = true;
 	networking.hostName = "nixpc";
 	networking.networkmanager.enable = true;
 
@@ -26,7 +25,7 @@
 		settings = {
 			PasswordAuthentication = true;
 			AllowUsers = null; # Allows all users by default. Can be [ "user1" "user2" ]
-			UseDns = true;
+				UseDns = true;
 			X11Forwarding = true;
 			PermitRootLogin = "no"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
 		};
@@ -72,20 +71,48 @@
 	programs.vim.defaultEditor = true;
 	programs.direnv.enable = true;
 	environment.systemPackages = with pkgs; [
+		ethtool
 		tree
-			vim
-			mutt
-			git 
-			mc
-			htop
-			tmux
-			ntfs3g
-			acpi
-			zip
-			unzip
-			xorg.xhost
-			man-pages
-			man-pages-posix
+		vim
+		mutt
+		git 
+		mc
+		htop
+		tmux
+		ntfs3g
+		acpi
+		zip
+		unzip
+		xorg.xhost
+		man-pages
+		man-pages-posix
 	];
+
+	services.samba = {
+		enable = true;
+		shares = {
+			myshare = {
+				path = "/srv/samba/myshare";  # Change this to your desired share path
+					writable = true;
+				browseable = true;
+				"guest ok" = "yes";  # Allow guest access
+			};
+		};
+	};
+
+# Ensure the directory exists and has proper permissions
+	systemd.tmpfiles.rules = [
+		"d /srv/samba/myshare 0777 root root -"
+	];
+
+	networking.firewall.allowedTCPPorts = [ 445 139 51820 ];
+	networking.firewall.allowedUDPPorts = [ 137 138 51820 ];
+	networking.firewall.enable = true;
+	networking.firewall.allowPing = true;
+	services.duckdns = {
+		enable = true;
+		domains = ["govnoobeseno"];
+		tokenFile = "/home/user/duckdns-token";
+	};
 }
 
