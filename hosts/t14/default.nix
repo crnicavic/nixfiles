8f9aebf6001db8c3a1a8d5c0efc8064b9dfa7193
crnicavic <cavic5000@tutanota.com>
2025-04-06 21:51:38 +0200

    a little more uniform structure

--- hosts/t14/default.nix
+++ hosts/t14/default.nix
@@ -1,5 +1,5 @@
 { config, lib, pkgs, ... }: {
-	imports = [ 
+	imports = [
 		./hardware-configuration.nix
 
 		../../packages.nix
@@ -17,43 +17,38 @@
 # Use the systemd-boot EFI boot loader.
 	boot.loader.systemd-boot.enable = true;
 	boot.loader.efi.canTouchEfiVariables = true;
-
 	boot.initrd.luks.devices.cryptroot.device = "/dev/disk/by-uuid/e8cf8f5d-0cfc-45fd-b9bd-265b662bff01";
+
+	# not very nix-like but works for me
+	environment.variables.PATH = [ "/usr/local/bin" ];
+
+	fileSystems."/".options = [ "noatime" "nodiratime" "discard" ];
+
+	hardware.bluetooth.enable = true;
+	hardware.bluetooth.powerOnBoot = true;
+
+	nixpkgs.config.allowUnfree = true;
+	nix.settings.experimental-features = [ "nix-command" "flakes"];
 
 	networking.hostName = "nix-t14";
 	networking.networkmanager.enable = true;
-#networking.firewall.enable = true;
-#	networking.firewall.allowPing = true;
-#	networking.firewall.allowedTCPPorts = [ 445 139 51820 ];
-#	networking.firewall.allowedUDPPorts = [ 137 138 51820 ];
 	
-	hardware.bluetooth.enable = true;
-	hardware.bluetooth.powerOnBoot = true;
-	time.timeZone = "Europe/Belgrade";
+	security.polkit.enable = true;
 
-	nix.settings.experimental-features = [ "nix-command" "flakes"];
-	system.autoUpgrade.enable = true;
-
-# Enable touchpad support
+	# Enable touchpad support
 	services.libinput.enable = true;
-
+	services.hardware.bolt.enable = true;
 	services.gvfs.enable = true;
-	nixpkgs.config.allowUnfree = true;
 	services.fwupd.enable = true;
 
-	services.hardware.bolt.enable = true;
+	system.autoUpgrade.enable = true;
+	system.stateVersion = "24.05"; 
+	
+	time.timeZone = "Europe/Belgrade";
 
 	users.users.user = {
 		isNormalUser = true;
 		extraGroups = [ "wheel" "audio" "video" "networkmanager"];
 	};
-	fileSystems."/".options = [ "noatime" "nodiratime" "discard" ];
-	security.polkit.enable = true;
-
-# not very nix-like but works for me
-	environment.variables.PATH = [ "/usr/local/bin" ];
-
-	system.stateVersion = "24.05"; 
-
 }
 
